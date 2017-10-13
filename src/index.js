53827418be06330d5bc317d3fb62778930893ef9
angel mendoza <mendozaangel003@gmail.com>
2017-10-13 13:35:14 -0400

    se arreglo lo del navbar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 class web {
-  constructor(body, navbar) {
-    this.body = body
-    this.nav = navbar
+  constructor() {
+    this.onClick()
+    this.body =  `<div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 m6 main-img">
+            <img class="responsive-img" src="img/rocket.png" alt="web-development">
+          </div>
+          <div class="col s12 m6 center-align main-text">
+            <h1 id="title">Programador</h1>
+            <div class="divider"></div>
+            <h2 id="sub-title">Mi trabajo es materializar tus proyectos utilizando la tecnologia.</h2>
+          </div>
+        </div>
+      </div>
+    </div>` 
+    this.render()
   }
   onClick(){
-    $(".nav-element").click(()=>{
-      let conten = $(".nav-element").val()
+    $(".nav-element").on('click', (event)=>{
+      let conten = $(event.currentTarget).data('href')
       alert(conten)
+      this.body = ''
+      this.render()
     })
+  }
+  render(){
+    $('.body-container').html(this.body) 
   }
 }
 
-$(".nav-element").click(()=>{
-  let conten = $("nav-element").html()
-  alert(conten)
-})
+new web()
